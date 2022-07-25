b3d7cfc1bb9341d35ef0e3d724d62142016b3606
pjshwa <pjshwa@gmail.com>
2022-07-25 10:07:59 +0900

    ADD 20220725

--- find-first-and-last-position-of-element-in-sorted-array.cc
+++ find-first-and-last-position-of-element-in-sorted-array.cc
@@ -1,12 +1,11 @@
 class Solution {
 public:
   vector<int> searchRange(vector<int>& nums, int target) {
-    int ub = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
-    int lb = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-    int n = nums.size();
-
-    vector<int> ans = {-1, -1};
-    if (ub - lb) ans[0] = lb, ans[1] = ub - 1;
-    return ans;
+    if (binary_search(nums.begin(), nums.end(), target)) {
+      int left = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+      int right = upper_bound(nums.begin(), nums.end(), target) - nums.begin() - 1;
+      return {left, right};
+    }
+    else return {-1, -1};
   }
 };
