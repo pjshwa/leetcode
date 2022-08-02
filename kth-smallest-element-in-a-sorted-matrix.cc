690f328bebfc4dfa5e5cb929f4938b046c1942f2
pjshwa <pjshwa@gmail.com>
2022-08-02 10:00:56 +0900

    ADD 20220802

---
+++ kth-smallest-element-in-a-sorted-matrix.cc
@@ -0,0 +1,19 @@
+class Solution {
+public:
+  int kthSmallest(vector<vector<int>>& matrix, int k) {
+    int n = matrix.size();
+    int left = matrix[0][0], right = matrix[n - 1][n - 1] + 1;
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      int cnt = 0;
+      for (int i = 0; i < n; i++) {
+        cnt += upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+      }
+
+      if (cnt < k) left = mid + 1;
+      else right = mid;
+    }
+
+    return left;
+  }
+};
