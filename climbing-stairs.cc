b5cc4c0a6a74c676610355bb92f084492b981d3f
pjshwa <pjshwa@gmail.com>
2022-12-12 09:56:51 +0900

    ADD 20221212

--- climbing-stairs.cc
+++ climbing-stairs.cc
@@ -1,8 +1,9 @@
 class Solution {
-  int d[46];
 public:
   int climbStairs(int n) {
-    d[0] = d[1] = 1;
+    vector<int> d(n + 1, 0);
+
+    d[0] = 1; d[1] = 1;
     for (int i = 2; i <= n; i++) d[i] = d[i - 1] + d[i - 2];
     return d[n];
   }
