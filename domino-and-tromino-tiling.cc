b6970b441fdc5f0b47e6a990cf14c7f915968033
pjshwa <pjshwa@gmail.com>
2022-12-24 09:47:39 +0900

    ADD 20221224

--- domino-and-tromino-tiling.cc
+++ domino-and-tromino-tiling.cc
@@ -1,12 +1,11 @@
-const int MOD = 1e9 + 7;
+const int MAX = 1000, MOD = 1e9 + 7;
 
 class Solution {
-  int d[1001];
 public:
   int numTilings(int n) {
-    memset(d, 0, sizeof(d));
+    vector<int> d(MAX + 1, 0);
     d[1] = 1, d[2] = 2, d[3] = 5;
-    for (int i = 4; i <= 1000; i++) {
+    for (int i = 4; i <= n; i++) {
       d[i] = (d[i] + d[i - 1]) % MOD;
       d[i] = (d[i] + d[i - 1]) % MOD;
       d[i] = (d[i] + d[i - 3]) % MOD;
