842d1956f606ec8af39077eea1b06cfe7f3ff8fa
pjshwa <pjshwa@gmail.com>
2022-12-26 09:57:01 +0900

    ADD 20221226

--- jump-game.cc
+++ jump-game.cc
@@ -1,10 +1,11 @@
 class Solution {
 public:
   bool canJump(vector<int>& nums) {
-    int i = 0, reach = 0, n = nums.size();
-    for (int i = 0; i < n && i <= reach; i++) {
+    int n = nums.size(), reach = nums[0];
+    for (int i = 0; i < n && reach >= i; ++i) {
       reach = max(reach, i + nums[i]);
+      if (reach >= n - 1) return true;
     }
-    return i == n;
+    return false;
   }
 };
