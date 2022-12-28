a5d184c5025f1674e6268bd77fb045c61b1cbfef
pjshwa <pjshwa@gmail.com>
2022-12-28 09:54:14 +0900

    ADD 20221228

---
+++ remove-stones-to-minimize-the-total.cc
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  int minStoneSum(vector<int>& piles, int k) {
+    priority_queue<int> pq;
+    for (int p : piles) pq.push(p);
+
+    while (k--) {
+      int t = pq.top(); pq.pop();
+      pq.push(t - t / 2);
+    }
+
+    int ans = 0;
+    while (!pq.empty()) {
+      ans += pq.top(); pq.pop();
+    }
+    return ans;
+  }
+};
