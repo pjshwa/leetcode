becb56f1594efaa4738e5718cfb3372c1d8c094d
pjshwa <pjshwa@gmail.com>
2022-12-31 09:42:58 +0900

    ADD 20221231

--- unique-paths-iii.cc
+++ unique-paths-iii.cc
@@ -1,42 +1,40 @@
-const int dx[4] = {0, 1, 0, -1}, dy[4] = {1, 0, -1, 0};
+const int dx[4] = {0, 0, 1, -1}, dy[4] = {1, -1, 0, 0};
 
 class Solution {
-  bool vis[20][20];
-  vector<vector<int>> grid;
-  int cc = 0, total = 0, ans = 0, si, sj, ei, ej, n, m;
+  int n, m, ei, ej, tot = 0, cur = 0, ans = 0;
+  bool vis[20][20] = {0};
 
   void dfs(int i, int j) {
+    vis[i][j] = true; cur++;
     if (i == ei && j == ej) {
-      if (cc == total - 1) ans++;
-      return;
+      if (cur == tot) ans++;
     }
+    else {
+      for (int k = 0; k < 4; k++) {
+        int x = i + dx[k], y = j + dy[k];
+        if (x < 0 || x >= n || y < 0 || y >= m || vis[x][y]) continue;
+        dfs(x, y);
+      }
 
-    vis[i][j] = true;
-    cc++;
-    for (int k = 0; k < 4; k++) {
-      int ni = i + dx[k], nj = j + dy[k];
-      if (ni < 0 || ni >= n || nj < 0 || nj >= m || vis[ni][nj] || grid[ni][nj] == -1)
-        continue;
-      dfs(ni, nj);
     }
-    vis[i][j] = false;
-    cc--;
+    vis[i][j] = false; cur--;
   }
+
 
 public:
   int uniquePathsIII(vector<vector<int>>& grid) {
-    this->grid = grid;
     n = grid.size(), m = grid[0].size();
-    
+
+    int si, sj;
     for (int i = 0; i < n; i++) for (int j = 0; j < m; j++) {
-      if (grid[i][j] != -1) total++;
       if (grid[i][j] == 1) si = i, sj = j;
       if (grid[i][j] == 2) ei = i, ej = j;
+
+      if (grid[i][j] == -1) vis[i][j] = true;
+      else tot++;
     }
 
-    memset(vis, 0, sizeof(vis));
     dfs(si, sj);
-
     return ans;
   }
 };
