1fe42dc205ec91d7f2d6ec73526ac680c6c8884e
pjshwa <pjshwa@gmail.com>
2023-01-05 14:20:39 +0900

    ADD 20230105

--- minimum-number-of-arrows-to-burst-balloons.cc
+++ minimum-number-of-arrows-to-burst-balloons.cc
@@ -1,19 +1,22 @@
 class Solution {
 public:
   int findMinArrowShots(vector<vector<int>>& points) {
-    int ans = 0, cs, ce;
-    bool first = true;
-    sort(points.begin(), points.end());
-    for (auto& v : points) {
-      int s = v[0], e = v[1];
-      if (first || s > ce) {
-        ans++;
-        first = false;
-        cs = s;
-        ce = e;
-      }
-      else cs = max(cs, s), ce = min(ce, e);
+    sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
+      if (a[0] == b[0]) return a[1] < b[1];
+      return a[0] < b[0];
+    });
+
+    int p = 1, n = points.size(), ans = 1, cs = points[0][0], ce = points[0][1];
+    while (p < n) {
+      int s = points[p][0], e = points[p][1];
+      int ns = max(cs, s), ne = min(ce, e);
+
+      if (ns <= ne) s = ns, e = ne;
+      else ans++;
+
+      cs = s; ce = e; p++;
     }
+
     return ans;
   }
 };
