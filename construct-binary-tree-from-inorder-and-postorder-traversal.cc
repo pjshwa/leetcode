a065af3b8558a40e2385bde34b218d4dc8e9cc55
pjshwa <pjshwa@gmail.com>
2023-03-16 11:14:09 +0900

    ADD 20230316

--- construct-binary-tree-from-inorder-and-postorder-traversal.cc
+++ construct-binary-tree-from-inorder-and-postorder-traversal.cc
@@ -1,30 +1,32 @@
-// struct TreeNode {
-//   int val;
-//   TreeNode *left;
-//   TreeNode *right;
-//   TreeNode() : val(0), left(nullptr), right(nullptr) {}
-//   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-// };
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-  TreeNode* buildNode(int is, int ie, int ps, int pe, vector<int>& inorder, vector<int>& postorder) {
-    TreeNode *node = new TreeNode(postorder[pe]);
-
-    // Find root position in inorder array
-    int pos = is;
-    while (inorder[pos] != postorder[pe]) pos++;
-
-    if (pos > is) node->left = buildNode(is, pos - 1, ps, pos - (is - ps) - 1, inorder, postorder);
-    if (pos < ie) node->right = buildNode(pos + 1, ie, pos - (is - ps), pe - 1, inorder, postorder);
-
-    return node;
-  }
-
   TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    function<TreeNode*(int, int, int, int)> dfs = [&](int il, int ir, int pl, int pr) {
+      if (il > ir) return (TreeNode*)nullptr;
+
+      int root = postorder[pr];
+      int k = il;
+      while (inorder[k] != root) ++k;
+
+      int left_size = k - il;
+      TreeNode* node = new TreeNode(root);
+      node->left = dfs(il, k - 1, pl, pl + left_size - 1);
+      node->right = dfs(k + 1, ir, pl + left_size, pr - 1);
+      return node;
+    };
+
     int n = inorder.size();
-    if (n) return buildNode(0, n - 1, 0, n - 1, inorder, postorder);
-    else return nullptr;
+    return dfs(0, n - 1, 0, n - 1);
   }
 };
