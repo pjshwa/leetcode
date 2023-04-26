9cb8e0b1cd1bce4a5c2e559810cacda41350de6d
pjshwa <pjshwa@gmail.com>
2023-04-26 10:23:12 +0900

    ADD 20230426

--- add-digits.cc
+++ add-digits.cc
@@ -1,13 +1,6 @@
 class Solution {
 public:
   int addDigits(int num) {
-    // if (num < 10) return num;
-
-    // string s = to_string(num);
-    // int nxt = 0;
-    // for (char& c : s) nxt += c - '0';
-    // return addDigits(nxt);
-    if (num == 0) return 0;
-    return (num - 1) % 9 + 1;
+    return num ? (num % 9 ? num % 9 : 9) : 0;
   }
 };
