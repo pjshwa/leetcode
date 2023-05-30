034c666fdced4b4b80d8c05b58a4509a537f09ca
pjshwa <pjshwa@gmail.com>
2023-05-30 09:51:44 +0900

    ADD 20230530

--- design-hashset.cc
+++ design-hashset.cc
@@ -1,18 +1,21 @@
+const int MAX = 1e6;
+
 class MyHashSet {
-  bool h[1'000'001];
+  bool h[MAX + 1];
+
 public:
   MyHashSet() {
     memset(h, 0, sizeof(h));
   }
-  
+
   void add(int key) {
     h[key] = true;
   }
-  
+
   void remove(int key) {
     h[key] = false;
   }
-  
+
   bool contains(int key) {
     return h[key];
   }
