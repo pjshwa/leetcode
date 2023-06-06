2538ff9fa4ec595f00a29943236cbc5b6cda4358
pjshwa <pjshwa@gmail.com>
2023-06-06 09:42:04 +0900

    ADD 20230606

---
+++ can-make-arithmetic-progression-from-sequence.cc
@@ -0,0 +1,11 @@
+class Solution {
+public:
+  bool canMakeArithmeticProgression(vector<int>& arr) {
+    sort(arr.begin(), arr.end());
+    int n = arr.size(), d = arr[1] - arr[0];
+    for (int i = 2; i < n; ++i) {
+      if (arr[i] - arr[i - 1] != d) return false;
+    }
+    return true;
+  }
+};
