cee90181aff504e9b7b6439e0dea7dddc0f13ac6
pjshwa <pjshwa@gmail.com>
2023-09-08 09:19:19 +0900

    ADD 20230908

--- pascals-triangle.cc
+++ pascals-triangle.cc
@@ -1,12 +1,21 @@
 class Solution {
 public:
   vector<vector<int>> generate(int numRows) {
-    vector<vector<int>> ans;
-    for (int i = 0; i < numRows; i++) {
-      vector<int> row(i + 1, 1);
-      for (int j = 1; j < i; j++) row[j] = ans[i - 1][j - 1] + ans[i - 1][j];
-      ans.push_back(row);
+    vector<vector<int>> result;
+    result.push_back({1});
+
+    for (int i = 1; i < numRows; ++i) {
+      vector<int> row;
+      for (int j = 0; j <= i; ++j) {
+        if (j == 0 || j == i) row.push_back(1);
+        else {
+          auto& prev_row = result[i - 1];
+          row.push_back(prev_row[j - 1] + prev_row[j]);
+        }
+      }
+      result.push_back(row);
     }
-    return ans;
+
+    return result;
   }
 };
