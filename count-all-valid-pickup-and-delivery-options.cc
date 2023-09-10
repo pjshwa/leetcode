726cc1bd43d0ff5cd4677868dcad3881da591214
pjshwa <pjshwa@gmail.com>
2023-09-10 12:30:09 +0900

    ADD 20230910

--- count-all-valid-pickup-and-delivery-options.cc
+++ count-all-valid-pickup-and-delivery-options.cc
@@ -3,10 +3,10 @@ const int MOD = 1e9 + 7;
 class Solution {
 public:
   int countOrders(int n) {
-    long long ans = 1;
-    for (int i = 1; i < n; i++) {
-      long long t = 2 * i + 1;
-      ans = (ans * ((t * (t + 1) / 2) % MOD)) % MOD;
+    int ans = 1;
+    for (int i = 1; i <= n; ++i) {
+      long long cur = i * (2 * i - 1);
+      ans = (ans * cur) % MOD;
     }
     return ans;
   }
