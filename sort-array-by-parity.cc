fa59bcdf5b37ef9561b88b692291f5f182b81635
pjshwa <pjshwa@gmail.com>
2023-09-28 09:33:07 +0900

    ADD 20230928

--- sort-array-by-parity.cc
+++ sort-array-by-parity.cc
@@ -1,16 +1,9 @@
 class Solution {
 public:
   vector<int> sortArrayByParity(vector<int>& nums) {
-    vector<int> e;
-    queue<int> o;
-    for (int i = 0; i < nums.size(); ++i) {
-      if (nums[i] % 2 == 0) e.push_back(nums[i]);
-      else o.push(nums[i]);
-    }
-    while (!o.empty()) {
-      e.push_back(o.front());
-      o.pop();
-    }
-    return e;
+    sort(nums.begin(), nums.end(), [](int a, int b) {
+      return a % 2 < b % 2;
+    });
+    return nums;
   }
 };
