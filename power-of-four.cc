a2ff64cda0be7795deb04c9496d2b7d9efa803be
pjshwa <pjshwa@gmail.com>
2023-10-23 20:40:29 +0900

    ADD 20231023

--- power-of-four.cc
+++ power-of-four.cc
@@ -1,10 +1,6 @@
 class Solution {
 public:
   bool isPowerOfFour(int n) {
-    if (n <= 0) return false;
-    n = abs(n);
-
-    bool pw2 = !(n & (n - 1));
-    return pw2 && (n & 0x55555555);
+    return n > 0 && !(n & (n - 1)) && (n & 0x55555555);
   }
 };
