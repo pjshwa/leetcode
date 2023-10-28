9198da87f4dc3f4db2f01f0619e70103f1411e54
pjshwa <pjshwa@gmail.com>
2023-10-28 09:19:22 +0900

    ADD 20231028

--- count-vowels-permutation.cc
+++ count-vowels-permutation.cc
@@ -1,21 +1,19 @@
 const int MOD = 1e9 + 7;
 
 class Solution {
-  int d[5][20001];
 public:
   int countVowelPermutation(int n) {
-    for (int i = 0; i < 5; i++) d[i][1] = 1;
+    vector<vector<int>> g = {{1, 2, 4}, {0, 2}, {1, 3}, {2}, {2, 3}};
+    vector<vector<int>> dp(n + 1, vector<int>(5));
+    for (int i = 0; i < 5; i++) dp[1][i] = 1;
     for (int j = 2; j <= n; j++) {
-      for (int i = 0; i < 5; i++) d[i][j] = 0;
-      for (int v : {1, 2, 4}) d[0][j] = (d[0][j] + d[v][j - 1]) % MOD;
-      for (int v : {0, 2}) d[1][j] = (d[1][j] + d[v][j - 1]) % MOD;
-      for (int v : {1, 3}) d[2][j] = (d[2][j] + d[v][j - 1]) % MOD;
-      for (int v : {2}) d[3][j] = (d[3][j] + d[v][j - 1]) % MOD;
-      for (int v : {2, 3}) d[4][j] = (d[4][j] + d[v][j - 1]) % MOD;
+      for (int i = 0; i < 5; i++) {
+        for (int k : g[i]) dp[j][i] = (dp[j][i] + dp[j - 1][k]) % MOD;
+      }
     }
 
     int ans = 0;
-    for (int i = 0; i < 5; i++) ans = (ans + d[i][n]) % MOD;
+    for (int i = 0; i < 5; i++) ans = (ans + dp[n][i]) % MOD;
     return ans;
   }
 };
