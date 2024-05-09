b30d0472f981cc60cd129c5b0bbdcc598506dc89
pjshwa <pjshwa@gmail.com>
2024-05-10 01:09:30 +0900

    ADD 20240509

---
+++ maximize-happiness-of-selected-children.cc
@@ -0,0 +1,9 @@
+class Solution {
+public:
+  long long maximumHappinessSum(vector<int>& happiness, int k) {
+    sort(happiness.begin(), happiness.end(), greater<int>());
+    long long ans = 0;
+    for (int i = 0; i < k; ++i) ans += max(0, happiness[i] - i);
+    return ans;
+  }
+};
