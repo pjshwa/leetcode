7c623b66aa81869cad421501f395b982a5c27c57
Jinsik Park <pjshwa@gmail.com>
2024-07-20 12:16:15 +0900

    ADD 20240720

--- f/find-valid-matrix-given-row-and-column-sums.cc
+++ f/find-valid-matrix-given-row-and-column-sums.cc
@@ -1,20 +1,14 @@
 class Solution {
 public:
-    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
-        int rsz = rowSum.size(), csz = colSum.size();
-
-        vector<vector<int>> ans;
-        for (int i = 0; i < rsz; i++) {
-            vector<int> row(csz, 0);
-            for (int j = 0; j < csz; j++) {
-                int cv = min(rowSum[i], colSum[j]);
-                row[j] = cv;
-                rowSum[i] -= cv;
-                colSum[j] -= cv;
-            }
-            ans.push_back(row);
-        }
-
-        return ans;
+  vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+    int N = rowSum.size(), M = colSum.size(), i = 0, j = 0;
+    vector<vector<int>> ans(N, vector<int>(M));
+    while (i < N && j < M) {
+      int val = min(rowSum[i], colSum[j]);
+      ans[i][j] = val; rowSum[i] -= val; colSum[j] -= val;
+      if (rowSum[i] == 0) ++i;
+      else ++j;
     }
+    return ans;
+  }
 };
