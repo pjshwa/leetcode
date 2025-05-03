e8e35e3cadc82f403c66705f7b5658e9b768d097
pjshwa <pjshwa@gmail.com>
2025-05-03 12:22:10 +0900

    ADD 20250503

--- m/minimum-domino-rotations-for-equal-row.cc
+++ m/minimum-domino-rotations-for-equal-row.cc
@@ -1,30 +1,19 @@
 class Solution {
 public:
   int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-    int n = tops.size();
-    map<int, int> t;
-    for (int i = 0; i < n; i++) {
-      t[tops[i]]++;
-      t[bottoms[i]]++;
-    }
+    int n = tops.size(), T[7] = {};
+    for (int i = 0; i < n; ++i) ++T[tops[i]], ++T[bottoms[i]];
 
-    int u = -1;
-    for (auto& [k, v] : t) {
-      if (v >= n) {
-        u = k;
-        break;
-      }
-    }
-    if (u == -1) return -1;
+    int u;
+    for (u = 1; u <= 6; ++u) if (T[u] >= n) break;
+    if (u == 7) return -1;
 
     int tto = 0, bto = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
       if (tops[i] != u && bottoms[i] != u) return -1;
       if (tops[i] == u && bottoms[i] == u) continue;
-      if (tops[i] == u) tto++;
-      else bto++;
+      tops[i] == u ? ++bto : ++tto;
     }
-
     return min(tto, bto);
   }
 };
