df5d6e56e41aacb7e52bc9d3f8fd6ba5614b3a2c
pjshwa <pjshwa@gmail.com>
2026-04-05 11:00:56 +0900

    ADD 20260405

---
+++ r/robot-return-to-origin.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+  bool judgeCircle(string moves) {
+    int x = 0, y = 0;
+    for (auto& c : moves) {
+      if (c == 'U') ++y;
+      if (c == 'D') --y;
+      if (c == 'L') --x;
+      if (c == 'R') ++x;
+    }
+    return x == 0 && y == 0;
+  }
+};
