9b29c6956395c46201f48e5ce8266187eb76c307
Jinsik Park <pjshwa@gmail.com>
2026-04-15 11:01:33 +0900

    ADD 20260415

--- _contests/wc325/shortest-distance-to-target-string-in-a-circular-array.cc
+++ _contests/wc325/shortest-distance-to-target-string-in-a-circular-array.cc
@@ -1,28 +1,11 @@
 class Solution {
 public:
-  int closetTarget(vector<string>& words, string target, int startIndex) {
-    vector<int> indices; int n = words.size();
-    for (int i = 0; i < n; ++i) {
-      string w = words[i];
-      if (w == target) indices.push_back(i);
+  int closestTarget(vector<string>& words, string target, int startIndex) {
+    int N = words.size(), ans = N;
+    for (int a = 0; a < N; ++a) {
+      int i = (startIndex + a) % N, j = (startIndex - a + N) % N;
+      if (words[i] == target || words[j] == target) return a;
     }
-    if (indices.empty()) return -1;
-
-    int ans = 0x3f3f3f3f;
-    for (int i = 0; i < indices.size(); ++i) {
-      int index = indices[i];
-      if (index > startIndex) {
-        ans = min(ans, index - startIndex);
-        ans = min(ans, n - index + startIndex);
-      }
-      else if (index < startIndex) {
-        ans = min(ans, startIndex - index);
-        ans = min(ans, n - startIndex + index)
-      }
-      else {
-        ans = min(ans, 0);
-      }
-    }
-    return ans;
+    return -1;
   }
 };
