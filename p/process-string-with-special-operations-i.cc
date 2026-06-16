0ad0078f575cc2f2d8b48527256a064a5216ea77
Jinsik Park <pjshwa@gmail.com>
2026-06-16 10:40:34 +0900

    ADD 20260616

---
+++ p/process-string-with-special-operations-i.cc
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  string processStr(string s) {
+    string result = "";
+    for (char c : s) {
+      if (c == '*') {
+        if (!result.empty()) result.pop_back();
+      } else if (c == '#') {
+        result += result;
+      } else if (c == '%') {
+        reverse(result.begin(), result.end());
+      } else {
+        result += c;
+      }
+    }
+    return result;
+  }
+};
