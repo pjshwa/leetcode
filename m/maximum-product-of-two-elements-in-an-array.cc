eb57f16df79093537080f04c08a752bad314ae1d
Jinsik Park <pjshwa@gmail.com>
2026-07-27 09:39:12 +0900

    ADD 20260727

--- m/maximum-product-of-two-elements-in-an-array.cc
+++ m/maximum-product-of-two-elements-in-an-array.cc
@@ -1,7 +1,9 @@
 class Solution {
 public:
   int maxProduct(vector<int>& nums) {
-    sort(nums.begin(), nums.end(), greater<>());
-    return (nums[0] - 1) * (nums[1] - 1);
+    sort(nums.begin(), nums.end());
+    int e1 = nums.back(); nums.pop_back();
+    int e2 = nums.back(); nums.pop_back();
+    return (e1 - 1) * (e2 - 1);
   }
 };
